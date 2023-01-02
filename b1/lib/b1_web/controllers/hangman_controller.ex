d8531a29b9391f4ccc01f3886f8aa393d1683684
Jose Martinez <44812411+JoseM111@users.noreply.github.com>
2023-01-01 20:53:24 -0500

    EPIC0005: status -> created a form for the app️ [Jan1st ]

--- b1/lib/b1_web/controllers/hangman_controller.ex
+++ b1/lib/b1_web/controllers/hangman_controller.ex
@@ -11,11 +11,45 @@ defmodule B1Web.HangmanController do
     # store the tally
     tally = Hangman.get_tally(game)
 
-    # cookie for session request
-    # Puts the specified in the session for the given .
-    put_session(conn, :game, game)
-    # Render the given template or the default template
-    # specified by the current action with the given assigns.
-    render(conn, "game.html", tally: tally)
+    conn
+    |> put_session(:game, game)
+    |> render("game.html", tally: tally)
+
+    #    # cookie for session request
+    #    # Puts the specified in the session for the given .
+    #    conn = put_session(conn, :game, game)
+    #    # Render the given template or the default template
+    #    # specified by the current action with the given assigns.
+    #    render(conn, "game.html", tally: tally)
+  end
+
+  def update(conn, params) do
+    #
+    guess = params[ "make_move" ][ "guess" ]
+
+    tally =
+      conn
+      |> get_session(:game)
+      |> Hangman.make_move(guess)
+
+    put_in(conn.params[ "make_move" ][ "guess" ], "")
+    |> render("game.html", tally: tally)
+
+    #    # getting the game session from
+    #    # the `put_session(conn, :game, game)`
+    #    game = get_session(conn, :game)
+    #
+    #    tally = Hangman.make_move(game, guess)
+    #    # making the form clear to an empty string after a guessed letter
+    #    conn = put_in(conn.params["make_move"]["guess"], "")
+    #
+    #    render(conn, "game.html", tally: tally)
   end
 end
+
+
+
+
+
+
+
