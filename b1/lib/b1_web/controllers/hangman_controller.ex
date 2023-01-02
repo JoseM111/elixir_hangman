8715ac71a2c747514167a499189670f130c91093
Jose Martinez <44812411+JoseM111@users.noreply.github.com>
2023-01-01 19:54:27 -0500

    EPIC0005: status -> integrated a new game to the app️ [Jan1st ]

--- b1/lib/b1_web/controllers/hangman_controller.ex
+++ b1/lib/b1_web/controllers/hangman_controller.ex
@@ -4,4 +4,18 @@ defmodule B1Web.HangmanController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def new(conn, _params) do
+    # creates pid
+    game = Hangman.new_game()
+    # store the tally
+    tally = Hangman.get_tally(game)
+
+    # cookie for session request
+    # Puts the specified in the session for the given .
+    put_session(conn, :game, game)
+    # Render the given template or the default template
+    # specified by the current action with the given assigns.
+    render(conn, "game.html", tally: tally)
+  end
 end
